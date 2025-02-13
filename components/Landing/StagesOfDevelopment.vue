c5b7083dd07b458787fe88c5278fa34be9841d3d
solomon <snisimov2005@mail.ru>
2025-02-13 20:25:22 +0500

    фигня карты

--- components/Landing/StagesOfDevelopment.vue
+++ components/Landing/StagesOfDevelopment.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts"></script>
-<template>сюда html</template>
-<style scoped lang="scss">
+<template>
+  <div class="container">
+    <h1>Сайты под ключ: разработка и запуск</h1>
 
+    <div class="features">
+      <div class="feature">
+        <h2>Полный цикл разработки</h2>
+        <p>От концепции до готового сайта.</p>
+      </div>
+
+      <div class="feature">
+        <h2>Адаптивный дизайн</h2>
+        <p>Корректное отображение на всех устройствах.</p>
+      </div>
+
+      <div class="feature">
+        <h2>SEO-оптимизация</h2>
+        <p>Повышение позиций в поисковых системах.</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.features {
+  display: flex;
+}
+.feature {
+  background-color: red;
+  height: 150px;
+  width: 400px;
+  border-radius: 8px;
+  margin: 10px;
+  padding-left: 10px;
+  padding-right: 5px;
+}
 </style>
