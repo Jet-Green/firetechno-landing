921564420548be1ab4e748031b30f689fbc07bcd
Jet-Green <grishadzyin@gmail.com>
2025-02-13 19:29:38 +0500

    init

---
+++ components/Landing/SiteDevelopmentForYourTasks.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+<template>
+  <v-row>
+    <v-col cols="6">
+      Разработка сайтов для ваших задач
+
+      <v-btn>Заказать</v-btn>
+    </v-col>
+
+    <v-col cols="6">
+      images
+    </v-col>
+  </v-row>
+</template>
+<style scoped lang="scss">
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container class="container">
+    <v-row class="d-flex justify-center">
+      <v-col cols="12" md="8" lg="7">
+        <LandingSiteDevelopmentForYourTasks />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<style scoped lang="scss">
+
+</style>
