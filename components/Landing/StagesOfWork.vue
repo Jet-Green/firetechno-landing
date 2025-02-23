2f01e33bf4527178b4d052f49b0902d0a9966f97
Jet-Green <grishadzyin@gmail.com>
2025-02-23 19:56:18 +0500

    add grouping stages icon

--- components/Landing/StagesOfWork.vue
+++ components/Landing/StagesOfWork.vue
@@ -1,10 +1,10 @@
 <script setup></script>
 <template>
-<div class="work">
-    <h2>Этапы работы: от идеи <br>
-        до реализации проекта</h2>
-    <img src="" alt="">
-    
-</div>
+  <div class="work">
+      <h2>Этапы работы: от идеи <br>
+          до реализации проекта</h2>
+          <img src="~/assets/icons/GroupingStages.svg" alt="" />
+      
+  </div>
 </template>
 <style scoped lang="scss"></style>
