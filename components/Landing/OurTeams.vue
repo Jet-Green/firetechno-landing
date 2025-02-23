9aab10801fc39d81a10c727e4f810abbf69aeab0
solomon <snisimov2005@mail.ru>
2025-02-23 20:47:10 +0500

    сщьфтв

--- components/Landing/OurTeams.vue
+++ components/Landing/OurTeams.vue
@@ -1,5 +1,66 @@
 <script setup></script>
 <template>
-наши команды
+  <h2></h2>
+  <v-col cols="12">
+    <v-row class="features">
+      <v-col cols="12" md="6" lg="4">
+        <div class="feature">
+          <v-row>
+            <v-col cols="8">
+              <p class="card-title">Интернет-Агенство</p>
+            </v-col>
+            <v-col cols="4" class="d-flex justify-end"> </v-col>
+          </v-row>
+
+          <p class="comment">От концепции до готового сайта.</p>
+        </div>
+      </v-col>
+
+      <v-col cols="12" md="6" lg="4">
+        <div class="feature">
+          <v-row>
+            <v-col cols="8">
+              <p class="card-title">Команда разработчиков</p>
+            </v-col>
+            <v-col cols="4" class="d-flex justify-end"> </v-col>
+          </v-row>
+
+          <p class="comment">От идеи до реализации.</p>
+        </div>
+      </v-col>
+
+      <v-col cols="12" md="6" lg="4">
+        <div class="feature">
+          <v-row>
+            <v-col cols="8">
+              <p class="card-title">Команда продвижения</p>
+            </v-col>
+            <v-col cols="4" class="d-flex justify-end"> </v-col>
+          </v-row>
+
+          <p class="comment">Повышение заинтересованности клиентов .</p>
+        </div>
+      </v-col>
+    </v-row>
+  </v-col>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.features {
+  display: flex;
+}
+.feature {
+  border: 1px solid #dc0000;
+  border-radius: 25px;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+}
+.comment {
+  margin-top: 40px;
+}
+.card-icon {
+  height: 80px;
+}
+</style>
