43ca09fc5ffe9b8947b0778f43001447d38173f3
solomon <snisimov2005@mail.ru>
2025-02-23 19:43:11 +0500

    qual

--- components/Landing/WhyUs.vue
+++ components/Landing/WhyUs.vue
@@ -1,6 +1,61 @@
 <script setup></script>
 <template>
-поч мы
+  <div class="cont">
+    <h2>
+      Наши преимущества: <br />
+      почему выбирают нас?
+    </h2>
+    <div class="exp">
+      <h4>Опыт</h4>
+      <p>
+        Многолетний опыт в сфере <br />
+        интернет-маркетинга.
+      </p>
+    </div>
+    <div class="ind">
+      <h4>Индивидуальный подход</h4>
+      <p>
+        Разработка решений, <br />
+        адаптированных под ваши задачи.
+      </p>
+    </div>
+    <div class="qual">
+      <h4>Качество</h4>
+      <p>
+        Гарантия высокого качества <br />
+        предоставляемых услуг.
+      </p>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
+.cont {
+  h4 {
+    padding-top: 74px;
+    padding-left: 74px;
+  }
+  .exp {
+    padding-right: 44px;
+    padding-bottom: 25px;
+    padding-left: 300px;
+    p {
+      padding-left: 74px;
+      padding-top: 20px;
+    }
+  }
+  .ind {
+    padding-left: 700px;
+    p {
+      padding-left: 74px;
+      padding-top: 20px;
+    }
+  }
+  .qual {
+    padding-left: 300px;
+    p {
+      padding-left: 74px;
+      padding-top: 20px;
+    }
+  }
+}
 </style>
