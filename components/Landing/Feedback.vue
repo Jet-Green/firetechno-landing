a797d256c97254ee9f9eec5bbdea0ce13ff30918
solomon <snisimov2005@mail.ru>
2025-02-23 21:40:54 +0500

    back info

--- components/Landing/Feedback.vue
+++ components/Landing/Feedback.vue
@@ -1,12 +1,47 @@
 <script setup></script>
 <template>
   <v-row>
+    <v-col cols="6"> тут первая половина(форма) </v-col>
     <v-col cols="6">
-      тут первая половина(форма)
+      <div class="back">
+        <h2>
+          Форма обратной связи: <br />
+          получите бесплатную <br />
+          консультацию
+        </h2>
+        <div class="inf">
+          <h4>
+            Заполните форму, и мы с вами свяжемся в <br />
+            ближайшее время.
+          </h4>
+          <p>
+            Оставьте свои контактные данные, и наш специалист <br />
+            проконсультирует вас по всем вопросам.
+          </p>
+        </div>
+        <div class="inf">
+          <h4>Узнайте, как мы можем помочь вашему бизнесу!</h4>
+          <p>
+            Мы предлагаем индивидуальные решения <br />
+            для каждого клиента.
+          </p>
+        </div>
+      </div>
     </v-col>
-    <v-col cols="6">
-      тут вторая половина
-    </v-col>
-  </v-row> 
+  </v-row>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.back {
+  padding-top: 89px;
+}
+.inf {
+  padding-top: 59px;
+  h4 {
+    padding-bottom: 19px;
+    font-size: 24px;
+  }
+  p {
+    font-size: 20px;
+  }
+}
+</style>
