0977fa338bec051f6121619d8da04852e4c2ee81
Jet-Green <grishadzyin@gmail.com>
2025-02-23 21:24:43 +0500

    finish stages of works

--- components/Landing/StagesOfWork.vue
+++ components/Landing/StagesOfWork.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <v-row style="margin-top: 150px;">
-    <v-col cols="12" style="margin-bottom: 100px;">
+    <v-col cols="12" style="margin-bottom: 70px;">
       <h2>
         Этапы работы: от идеи <br />
         до реализации проекта
@@ -11,37 +11,37 @@
 
     <v-col cols="12">
       <v-row class="all__comp">
-        <v-col class="number">
+        <v-col class="number" cols="12" sm="6" lg="3">
           <span>1</span>
-          <p class="card-title">Автоматизация</p>
-          <p>
+          <p class="card-title mb-4">Автоматизация</p>
+          <p style="margin-top: auto;">
             Ответы на часто <br />
             задаваемые вопросы.
           </p>
         </v-col>
-        <v-col class="number">
+        <v-col class="number" cols="12" sm="6" lg="3">
           <span>2</span>
-          <p class="card-title">
+          <p class="card-title mb-4">
             Круглосуточная <br />
             поддержка
           </p>
-          <p>
+          <p style="margin-top: auto;">
             Доступность для <br />
             клиентов в любое время.
           </p>
         </v-col>
-        <v-col class="number">
+        <v-col class="number" cols="12" sm="6" lg="3">
           <span>3</span>
-          <p class="card-title">Вовлеченность</p>
-          <p>
+          <p class="card-title mb-4">Вовлеченность</p>
+          <p style="margin-top: auto;">
             Повышение лояльности и <br />
             удовлетворенности клиентов.
           </p>
         </v-col>
-        <v-col class="number">
+        <v-col class="number" cols="12" sm="6" lg="3">
           <span>4</span>
-          <p class="card-title">Результат</p>
-          <p>
+          <p class="card-title mb-4">Результат</p>
+          <p style="margin-top: auto;">
             Довольный клиент и <br />
             готовое решение для бизнеса
           </p>
