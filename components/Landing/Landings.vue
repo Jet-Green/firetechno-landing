ff337a5a7ac55b3a21f2292f1c935954fe9f33c5
solomon <snisimov2005@mail.ru>
2025-02-23 18:22:59 +0500

    text

--- components/Landing/Landings.vue
+++ components/Landing/Landings.vue
@@ -1,6 +1,52 @@
 <script setup></script>
 <template>
-  sdf
+  <div class="text_photo">
+    <div class="text">
+      <h2>Лендинги, которые конвертируют <br />посетителей в клиентов</h2>
+      <ul class="baze">
+        <li class="baze_text">
+          <h4>Привлекательный дизайн</h4>
+          <p>Визуально убедительное <br />представление вашего продукта.</p>
+        </li>
+        <li class="baze_text">
+          <h4>Четкий призыв к действию</h4>
+          <p>
+            Мотивация посетителей к совершению <br />
+            целевого действия.
+          </p>
+        </li>
+        <li class="baze_text">
+          <h4>Аналитика и оптимизация</h4>
+          <p>
+            Отслеживание результатов и улучшение<br />
+            показателей.
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div class="photo">
+      <img src="" alt="" />
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
+.text {
+  h2 {
+    size: 40px;
+    padding-top: 94px;
+    padding-left: 74px;
+  }
+  .baze {
+    padding-top: 107px;
+    padding-bottom: 41px;
+    padding-left: 74px;
+    list-style: none;
+
+    h4 {
+      margin-bottom: 20px;
+      size: 30px;
+      margin-top: 94px;
+    }
+  }
+}
 </style>
