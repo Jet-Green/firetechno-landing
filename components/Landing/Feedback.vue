8937a43fa05b35983c11cf029cac3607a5619aa5
Jet-Green <grishadzyin@gmail.com>
2025-02-25 23:15:07 +0500

    add public

--- components/Landing/Feedback.vue
+++ components/Landing/Feedback.vue
@@ -37,7 +37,7 @@ const handleSubmit = async () => {
           "Бюджет": toSend.budget,
           "Комментарий": toSend.comment,
         },
-        apiKey: config.formToMailApiKey,
+        apiKey: config.public.formToMailApiKey,
       })
     });
 
