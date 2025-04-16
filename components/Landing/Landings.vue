b0c8848d5539080285bf63c3f47dd30d09275bd4
Jet-Green <grishadzyin@gmail.com>
2025-04-16 18:53:41 +0500

    add icons

--- components/Landing/Landings.vue
+++ components/Landing/Landings.vue
@@ -8,18 +8,27 @@
     <v-col cols="12" lg="6">
       <div class="list">
         <div class="">
-          <p class="card-title mb-6">Привлекательный дизайн</p>
+          <p class="card-title mb-6">
+            <v-icon>mdi-airplane-takeoff</v-icon>
+            Привлекательный дизайн
+          </p>
           <p class="mb-10">Визуально убедительное <br />представление вашего продукта.</p>
         </div>
         <div class="">
-          <p class="card-title mb-6">Четкий призыв к действию</p>
+          <p class="card-title mb-6">
+            <v-icon>mdi-bullseye-arrow</v-icon>
+            Четкий призыв к действию
+          </p>
           <p class="mb-10">
             Мотивация посетителей к совершению <br />
             целевого действия.
           </p>
         </div>
         <div class="">
-          <p class="card-title mb-6">Аналитика и оптимизация</p>
+          <p class="card-title mb-6">
+            <v-icon>mdi-google-analytics</v-icon>
+            Аналитика и оптимизация
+          </p>
           <p class="mb-10">
             Отслеживание результатов и улучшение<br />
             показателей.
@@ -41,18 +50,18 @@
   </v-row>
 </template>
 <style scoped lang="scss">
-  .list {
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    height: 100%;
-  }
+.list {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+}
 
-  .man-with-comp {
-    height: 80vh;
-    position: absolute;
-    left: -200px;
-    top: -200px;
-  }
+.man-with-comp {
+  height: 80vh;
+  position: absolute;
+  left: -200px;
+  top: -200px;
+}
 </style>
